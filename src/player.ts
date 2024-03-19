6cef6d61a8009204a1b53065621921c59132b1d8
bi3mer <colan@comcast.net>
2024-03-19 10:19:20 -0400

    player rectangle animates based on the direction they are moving. I also made it so the longer the press, the more pronunced the angle of the rectangle gets. This is only to a certain degree, so I would say it's more likely to not be noticed by the player, but hopefully they can kind of intuit the feel, if that makes sense. Sounds kind of pretentious when I read it back.

--- src/player.ts
+++ src/player.ts
@@ -4,8 +4,11 @@ import { InputManager, Key } from "./inputManager";
 
 const PLAYER_WIDTH = 20;
 const PLAYER_HEIGHT = 30;
-
+const MAX_MOVE_MOD = 8;
 export class Player extends GameObject {
+  private movingRight: boolean = false;
+  private movingLeft: boolean = false;
+  private moveMod: number = 0;
 
   constructor(x: number, y: number) {
     super(x, y);
@@ -14,16 +17,51 @@ export class Player extends GameObject {
   update(dt: number): void {
 
     if (InputManager.isKeyDown(Key.D, Key.RIGHT)) {
+      this.movingRight = true;
       this.x += 4;
+      this.moveMod = Math.min(MAX_MOVE_MOD, this.moveMod + 1);
     }
 
     if (InputManager.isKeyDown(Key.A, Key.LEFT)) {
+      this.movingLeft = true;
       this.x -= 4;
+      this.moveMod = Math.min(MAX_MOVE_MOD, this.moveMod + 1);
+      // this.moveMod = Math.max(-MAX_MOVE_MOD, this.moveMod - 1);
+    }
+
+    if (this.movingLeft && this.movingRight) {
+      this.movingLeft = false;
+      this.movingRight = false;
+      this.moveMod = 0;
     }
   }
 
   render(ctx: CanvasRenderingContext2D): void {
-    ctx.fillStyle = "rgba(100,100,255,1)";
-    ctx.fillRect(this.x, this.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+    ctx.fillStyle = "rgba(150,150,255,1)";
+
+    if (this.movingRight) {
+      let region = new Path2D();
+      region.moveTo(this.x, this.y);
+      region.lineTo(this.x - this.moveMod, this.y + PLAYER_HEIGHT);
+      region.lineTo(this.x + PLAYER_WIDTH - this.moveMod, this.y + PLAYER_HEIGHT);
+      region.lineTo(this.x + PLAYER_WIDTH, this.y);
+      region.closePath();
+
+      ctx.fill(region, "evenodd");
+
+      this.movingRight = false;
+    } else if (this.movingLeft) {
+      let region = new Path2D();
+      region.moveTo(this.x, this.y);
+      region.lineTo(this.x + this.moveMod, this.y + PLAYER_HEIGHT);
+      region.lineTo(this.x + PLAYER_WIDTH + this.moveMod, this.y + PLAYER_HEIGHT);
+      region.lineTo(this.x + PLAYER_WIDTH, this.y);
+      region.closePath();
+
+      ctx.fill(region, "evenodd");
+      this.movingLeft = false;
+    } else {
+      ctx.fillRect(this.x, this.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+    }
   }
 } 
