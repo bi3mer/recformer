45ecaa050117c07680cc0b5b726b89a5e5045cc9
bi3mer <colan@comcast.net>
2024-03-20 18:00:17 -0400

    I decided to have two arrays. ONe for dynamic entitites and one for static. It's a tiny improvement to the code base that just makes me like the code better, but isn't a big improvement. I still have to handle removing entities from the quadtree and the list...

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -16,25 +16,28 @@ export class GameScene extends Scene {
   private camera: Camera;
   private numCoins: number;
 
-  private entities: GameObject[];
+  private staticEntities: GameObject[];
+  private dynamicEntities: GameObject[];
 
   constructor(ctx: CanvasRenderingContext2D) {
     super();
 
     this.ctx = ctx;
     this.camera = new Camera();
-    this.entities = [];
   }
 
   onEnter(): void {
+    this.dynamicEntities = [];
+    this.staticEntities = [];
+
     this.numCoins = 0;
     this.quadTree = new QuadTree(
       new Point(0, 0),
       new Point(tempLVL[0].length * TILE_SIZE, 15 * TILE_SIZE)
     );
 
-    this.entities.push(new Player(2, 12, this.quadTree)); // player is always the first entity 
-    this.quadTree.insert(this.entities[0]);
+    this.dynamicEntities.push(new Player(2, 12, this.quadTree)); // player is always the first entity 
+    this.quadTree.insert(this.dynamicEntities[0]);
     const lvl = tempLVL;
     const rows = lvl.length;
     if (rows !== NUM_ROWS) {
@@ -53,12 +56,12 @@ export class GameScene extends Scene {
       for (let col = 0; col < columns; ++col) {
         if (row[col] === 'X') {
           const b = new Block(col, r);
-          this.entities.push(b);
+          this.staticEntities.push(b);
           this.quadTree.insert(b);
         } else if (row[col] === 'o') {
           const c = new Coin(col, r);
           ++this.numCoins;
-          this.entities.push(c);
+          this.dynamicEntities.push(c);
           this.quadTree.insert(c);
         }
       }
@@ -68,9 +71,9 @@ export class GameScene extends Scene {
   }
 
   update(dt: number): void {
-    const size = this.entities.length;
+    const size = this.dynamicEntities.length;
     for (let i = 0; i < size; ++i) {
-      this.entities[i].update(dt);
+      this.dynamicEntities[i].update(dt);
     }
 
     this.quadTree.update();
@@ -78,13 +81,14 @@ export class GameScene extends Scene {
 
     // TODO: handle entity removal, especially for coins
 
-    if ((this.entities[0] as Player).coinsCollected === this.numCoins) {
+    const player = this.dynamicEntities[0] as Player;
+    if (player.coinsCollected === this.numCoins) {
       console.log("Player won!");
       this.changeScene = KEY_MAIN_MENU;
     }
 
     // if the player is dead, we're done
-    if ((this.entities[0] as Player).isDead) {
+    if (player.isDead) {
       this.changeScene = KEY_MAIN_MENU;
     }
   }
@@ -93,17 +97,22 @@ export class GameScene extends Scene {
     this.ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     // Update camera view based on the player before rendering
-    this.camera.update(this.entities[0].pos.x);
+    this.camera.update(this.dynamicEntities[0].pos.x);
 
     // render entitites
-    const size = this.entities.length;
-    for (let i = 0; i < size; ++i) {
-      this.entities[i].render(this.ctx, this.camera);
+    let size = this.staticEntities.length;
+    let i = 0;
+    for (; i < size; ++i) {
+      this.staticEntities[i].render(this.ctx, this.camera);
+    }
+
+    size = this.dynamicEntities.length;
+    for (i = 0; i < size; ++i) {
+      this.dynamicEntities[i].render(this.ctx, this.camera);
     }
   }
 
   protected _onExit(): void {
-    this.entities = []; // remove entities
-    this.quadTree
+
   }
 }
