1fd6bdace5399eecfdcb3f93e31be8f43801801d
bi3mer <colan@comcast.net>
2024-03-21 14:41:50 -0400

    I forgot to remove some pointless console logs from the player. I also added some animation to the coins :D

--- src/coin.ts
+++ src/coin.ts
@@ -4,13 +4,25 @@ import { GameObject } from "./gameObject";
 import { TYPE_COIN, TYPE_PLAYER } from "./gameObjectTypes";
 
 export class Coin extends GameObject {
+  minY: number;
+  maxY: number;
+  yMod: number;
+
   constructor(x: number, y: number) {
     super(x + 0.25, y + 0.25, COIN_WIDTH, COIN_HEIGHT, TYPE_COIN);
     this.gravity.y = 0;
+    this.yMod = Math.random() * 0.5;
+    this.maxY = y + 0.3;
+    this.minY = y + 0.15;
+    this.velocity.y = this.yMod;
   }
 
   update(dt: number): void {
-
+    if (this.pos.y >= this.maxY) {
+      this.velocity.y = -this.yMod;
+    } else if (this.pos.y <= this.minY) {
+      this.velocity.y = this.yMod;
+    }
   }
 
   handleCollision(other: GameObject): void {
