40e5ffe5b2334fc4f5378ed28d28f453a3d4ed50
bi3mer <colan@comcast.net>
2025-01-13 10:39:11 -0500

    get levels implemented

--- server_src/socketServer.ts
+++ server_src/socketServer.ts
@@ -1,4 +1,5 @@
 import { CONFIG } from "./config";
+import { MDP, idToLevel } from "../src/levels";
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
@@ -9,7 +10,24 @@ const server = Bun.listen({
     data(socket, data) {
       const request = decoder.decode(data);
       if (request == "config") {
+        console.log("sending config.");
         socket.write(encoder.encode(JSON.stringify(CONFIG)));
+        socket.persist();
+      } else if (request === "levels") {
+        console.log("sending levels.");
+
+        // @TODO: this will fail for new MDP format
+        const L: string[][] = [];
+        for (const l of Object.keys(MDP.nodes)) {
+          const level = idToLevel[l];
+          if (level !== undefined) {
+            L.push(level);
+          }
+        }
+
+        socket.write(encoder.encode(JSON.stringify(L) + "EOF"));
+      } else if (request.substring(0, 6) === "assess") {
+        console.log("assess not yet implemented...");
       } else {
         console.log(`Unhandled request type: ${request}`);
       }
